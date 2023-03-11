25157d3e1b1f1015080604835ecbca7cb084261a
Yulia Rychkova <kurschool37@list.ru>
2023-03-12 04:41:39 +0700

    Initial project Add homework for lesson 1

---
+++ lesson1/Homework1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите имя");
+        string S1name = Console.ReadLine();
+        Console.WriteLine("Привет" + S1name);
+        Console.ReadLine();
+    }
+}
+            
