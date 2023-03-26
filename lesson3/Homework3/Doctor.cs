ec75a1250e08b0c9b33aadfda6163e06de26ceb0
Yulia Rychkova <kurschool37@list.ru>
2023-03-26 22:35:30 +0700

    Initial project Add homework for lesson 3

---
+++ lesson3/Homework3/Doctor.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Homework3
+{
+    public class Doctor
+    {
+        public Doctor(string name, int age, string special, int exp)
+        {
+            Name = name;
+            Age = age;
+            Special = special;
+            Exp = exp;
+        }
+        public string Name { get; set;}
+        public int Age { get; set; }
+        public string Special { get; set; }
+        public int Exp { get; set; }
+
+        public void Showinfo()
+        {
+            Console.WriteLine(Name, Special);
+        }
+
+        public void cure(Patient patient)
+        {
+            if (patient.Status == "болен")
+            {
+                patient.Status = "здоров";
+            } 
+           
+        }
+    }
+}
---
+++ lesson3/Homework3/Program.cs
@@ -0,0 +1,14 @@
+﻿using Homework3;
+
+public class Program
+{
+    public static void Main()
+    {
+        var doctor1 = new Doctor ( "Иванов И.И.", 40, "Терапевт", 6 );
+        var doctor2 = new Doctor("Сидоров М.В.", 38, "Окулист", 10);
+        var doctor3 = new Doctor("Петров В.В.", 32, "Кардиолог", 4);
+        var patient1 = new Patient("Крапская Ф.А", 24, "Коли Мяготина, 35", "здоров");
+        var patient2 = new Patient("Кубик П.В", 30, "Гоголя, 52","болен");
+
+    }
+}
