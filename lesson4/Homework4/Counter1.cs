c44937561c2eeabb9851d65ce61ab9767e7c3604
Yulia Rychkova <kurschool37@list.ru>
2023-04-09 04:37:41 +0700

    Inital project Add homework for lesson 4

---
+++ lesson4/Homework4/Counter1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+     class Counter1
+    {
+        public delegate void MethodContainer();
+        public event MethodContainer OnCount;
+        public void Count()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if(i== 77)
+                { 
+                    if (OnCount != null)
+                    {
+                        OnCount();
+                    }   
+                }
+            }
+        }
+    }
+}
